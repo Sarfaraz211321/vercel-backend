befd9775684b6b44ab823bb65e49b7edede14370
unknown <mohdsarfarazkhan237@gmail.com>
2025-11-07 09:43:07 +0530

    database changed

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
  export const dbConnect=async()=>{
-  const conn=await mongoose.connect('mongodb://localhost:27017/Querix');
+  const conn=await mongoose.connect('mongodb+srv://sizu4292_db_user:ls0LtiveEgxOpxDp@cluster0.guhepl3.mongodb.net/?appName=Cluster0');
   if(conn){
     console.log("database has connected successfully....")
   }
